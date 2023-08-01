81b07bb74b6b633c97c2b7503eb16177b4c1f1bc
kalema1 <kalemar8@gmail.com>
2023-08-01 15:20:09 +0300

    get card id

--- tasks/strings.js
+++ tasks/strings.js
@@ -335,8 +335,17 @@ function concatenateStrings(value1, value2) {
    *   'K♠' => 51
    */
   function getCardId(value) {
-    throw new Error('Not implemented');
+    const arr = [
+      'A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣',
+      'A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦',
+      'A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥',
+      'A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠'
+    ]
+    const index = arr.indexOf(value);
+    return index;
   }
+  let i = getCardId('K♠');
+  console.log(i);
   
   module.exports = {
     concatenateStrings: concatenateStrings,
