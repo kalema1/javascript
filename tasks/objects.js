cf567f47311646ad3f305840d19846d63c684c8d
kalema1 <kalemar8@gmail.com>
2023-09-22 22:34:15 +0300

    objects

--- tasks/objects.js
+++ tasks/objects.js
@@ -1,4 +1,3 @@
-
 /** ************************************************************************************************
  *                                                                                                *
  * Plese read the following tutorial before implementing tasks:                                   *
@@ -6,7 +5,6 @@
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object        *
  *                                                                                                *
  ************************************************************************************************ */
-
 
 /**
  * Returns the rectagle object with width and height parameters and getArea() method
@@ -22,131 +20,153 @@
  *    console.log(r.getArea());   // => 200
  */
 function Rectangle(width, height) {
-    throw new Error('Not implemented');
-  }
-  
-  
-  /**
-   * Returns the JSON representation of specified object
-   *
-   * @param {object} obj
-   * @return {string}
-   *
-   * @example
-   *    [1,2,3]   =>  '[1,2,3]'
-   *    { width: 10, height : 20 } => '{"height":10,"width":20}'
-   */
-  function getJSON(obj) {
-    throw new Error('Not implemented');
-  }
-  
-  
-  /**
-   * Returns the object of specified type from JSON representation
-   *
-   * @param {Object} proto
-   * @param {string} json
-   * @return {object}
-   *
-   * @example
-   *    var r = fromJSON(Rectangle.prototype, '{"width":10, "height":20}');
-   *
-   */
-  function fromJSON(proto, json) {
-    throw new Error('Not implemented');
-  }
-  
-  
-  /**
-   * Css selectors builder
-   *
-   * Each complex selector can consists of type, id, class, attribute, pseudo-class and
-   * pseudo-element selectors:
-   *
-   *    element#id.class[attr]:pseudoClass::pseudoElement
-   *              \----/\----/\----------/
-   *              Can be several occurences
-   *
-   * All types of selectors can be combined using the combinators ' ','+','~','>' .
-   *
-   * The task is to design a single class, independent classes or classes hierarchy and
-   * implement the functionality
-   * to build the css selectors using the provided cssSelectorBuilder.
-   * Each selector should have the stringify() method to output the string repsentation
-   * according to css specification.
-   *
-   * Provided cssSelectorBuilder should be used as facade only to create your own classes,
-   * for example the first method of cssSelectorBuilder can be like this:
-   *   element: function(value) {
-   *       return new MySuperBaseElementSelector(...)...
-   *   },
-   *
-   * The design of class(es) is totally up to you, but try to make it as simple, clear
-   * and readable as possible.
-   *
-   * @example
-   *
-   *  var builder = cssSelectorBuilder;
-   *
-   *  builder.id('main').class('container').class('editable').stringify() =>
-   *    '#main.container.editable'
-   *
-   *  builder.element('a').attr('href$=".png"').pseudoClass('focus').stringify() =>
-   *    'a[href$=".png"]:focus'
-   *
-   *  builder.combine(
-   *      builder.element('div').id('main').class('container').class('draggable'),
-   *      '+',
-   *      builder.combine(
-   *          builder.element('table').id('data'),
-   *          '~',
-   *           builder.combine(
-   *               builder.element('tr').pseudoClass('nth-of-type(even)'),
-   *               ' ',
-   *               builder.element('td').pseudoClass('nth-of-type(even)')
-   *           )
-   *      )
-   *  ).stringify() =>
-   *      'div#main.container.draggable + table#data ~ tr:nth-of-type(even) td:nth-of-type(even)'
-   *
-   *  For more examples see unit tests.
-   */
-  
-  const cssSelectorBuilder = {
-  
-    element(value) {
-      throw new Error('Not implemented');
+  return {
+    width: width,
+    height: height,
+    getArea: function () {
+      return this.width * this.height;
     },
-  
-    id(value) {
-      throw new Error('Not implemented');
-    },
-  
-    class(value) {
-      throw new Error('Not implemented');
-    },
-  
-    attr(value) {
-      throw new Error('Not implemented');
-    },
-  
-    pseudoClass(value) {
-      throw new Error('Not implemented');
-    },
-  
-    pseudoElement(value) {
-      throw new Error('Not implemented');
-    },
-  
-    combine(selector1, combinator, selector2) {
-      throw new Error('Not implemented');
-    }
   };
-  
-  module.exports = {
-    Rectangle: Rectangle,
-    getJSON: getJSON,
-    fromJSON: fromJSON,
-    cssSelectorBuilder: cssSelectorBuilder
-  };
-  
+}
+var r = new Rectangle(10, 20);
+console.log(r.width);
+console.log(r.getArea());
+
+/**
+ * Returns the JSON representation of specified object
+ *
+ * @param {object} obj
+ * @return {string}
+ *
+ * @example
+ *    [1,2,3]   =>  '[1,2,3]'
+ *    { width: 10, height : 20 } => '{"height":10,"width":20}'
+ */
+function getJSON(obj) {
+  return JSON.stringify(obj);
+}
+console.log(getJSON({ width: 10, height: 20 }));
+
+/**
+ * Returns the object of specified type from JSON representation
+ *
+ * @param {Object} proto
+ * @param {string} json
+ * @return {object}
+ *
+ * @example
+ *    var r = fromJSON(Rectangle.prototype, '{"width":10, "height":20}');
+ *
+ */
+function fromJSON(proto, json) {
+  return JSON.parse(json, proto);
+}
+var r = fromJSON(Rectangle.prototype, '{"width":10, "height":20}');
+console.log(r);
+
+/**
+ * Css selectors builder
+ *
+ * Each complex selector can consists of type, id, class, attribute, pseudo-class and
+ * pseudo-element selectors:
+ *
+ *    element#id.class[attr]:pseudoClass::pseudoElement
+ *              \----/\----/\----------/
+ *              Can be several occurences
+ *
+ * All types of selectors can be combined using the combinators ' ','+','~','>' .
+ *
+ * The task is to design a single class, independent classes or classes hierarchy and
+ * implement the functionality
+ * to build the css selectors using the provided cssSelectorBuilder.
+ * Each selector should have the stringify() method to output the string repsentation
+ * according to css specification.
+ *
+ * Provided cssSelectorBuilder should be used as facade only to create your own classes,
+ * for example the first method of cssSelectorBuilder can be like this:
+ *   element: function(value) {
+ *       return new MySuperBaseElementSelector(...)...
+ *   },
+ *
+ * The design of class(es) is totally up to you, but try to make it as simple, clear
+ * and readable as possible.
+ *
+ * @example
+ *
+ *  var builder = cssSelectorBuilder;
+ *
+ *  builder.id('main').class('container').class('editable').stringify() =>
+ *    '#main.container.editable'
+ *
+ *  builder.element('a').attr('href$=".png"').pseudoClass('focus').stringify() =>
+ *    'a[href$=".png"]:focus'
+ *
+ *  builder.combine(
+ *      builder.element('div').id('main').class('container').class('draggable'),
+ *      '+',
+ *      builder.combine(
+ *          builder.element('table').id('data'),
+ *          '~',
+ *           builder.combine(
+ *               builder.element('tr').pseudoClass('nth-of-type(even)'),
+ *               ' ',
+ *               builder.element('td').pseudoClass('nth-of-type(even)')
+ *           )
+ *      )
+ *  ).stringify() =>
+ *      'div#main.container.draggable + table#data ~ tr:nth-of-type(even) td:nth-of-type(even)'
+ *
+ *  For more examples see unit tests.
+ */
+
+const cssSelectorBuilder = {
+  value: "",
+  element(value) {
+    this.value += value;
+    return this;
+  },
+
+  id(value) {
+    this.value = `${this.value}#${value}`;
+    return this;
+  },
+
+  class(value) {
+    this.value = `${this.value}.${value}`;
+    return this;
+  },
+
+  attr(value) {
+    this.value = `${this.value}[${value}]`;
+    return this;
+  },
+
+  pseudoClass(value) {
+    this.value = `${this.value}:${value}`;
+    return this;
+  },
+
+  pseudoElement(value) {
+    this.value = `${this.value}::${value}`;
+    return this;
+  },
+
+  combine(selector1, combinator, selector2) {
+    this.value = `${selector1.value} ${combinator} ${selector2.value}`;
+    return this;
+  },
+  stringify() {
+    return this.value;
+  },
+};
+const builder = cssSelectorBuilder;
+console.log(
+  builder.element("div").class("container").class("clickable").stringify()
+);
+
+module.exports = {
+  Rectangle: Rectangle,
+  getJSON: getJSON,
+  fromJSON: fromJSON,
+  cssSelectorBuilder: cssSelectorBuilder,
+};
